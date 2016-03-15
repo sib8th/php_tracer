984d4bdd7a5c5a853fba521d53495c5a9c9bf6ad
sib8th <sib8th@163.com>
2016-03-15 17:23:04 +0800

    First commit

---
+++ config.m4
@@ -0,0 +1,63 @@
+dnl $Id$
+dnl config.m4 for extension php_tracer
+
+dnl Comments in this file start with the string 'dnl'.
+dnl Remove where necessary. This file will not work
+dnl without editing.
+
+dnl If your extension references something external, use with:
+
+PHP_ARG_WITH(php_tracer, for php_tracer support,
+dnl Make sure that the comment is aligned:
+[  --with-php_tracer             Include php_tracer support])
+
+dnl Otherwise use enable:
+
+dnl PHP_ARG_ENABLE(php_tracer, whether to enable php_tracer support,
+dnl Make sure that the comment is aligned:
+dnl [  --enable-php_tracer           Enable php_tracer support])
+
+if test "$PHP_PHP_TRACER" != "no"; then
+  dnl Write more examples of tests here...
+
+  dnl # --with-php_tracer -> check with-path
+  dnl SEARCH_PATH="/usr/local /usr"     # you might want to change this
+  dnl SEARCH_FOR="/include/php_tracer.h"  # you most likely want to change this
+  dnl if test -r $PHP_PHP_TRACER/$SEARCH_FOR; then # path given as parameter
+  dnl   PHP_TRACER_DIR=$PHP_PHP_TRACER
+  dnl else # search default path list
+  dnl   AC_MSG_CHECKING([for php_tracer files in default path])
+  dnl   for i in $SEARCH_PATH ; do
+  dnl     if test -r $i/$SEARCH_FOR; then
+  dnl       PHP_TRACER_DIR=$i
+  dnl       AC_MSG_RESULT(found in $i)
+  dnl     fi
+  dnl   done
+  dnl fi
+  dnl
+  dnl if test -z "$PHP_TRACER_DIR"; then
+  dnl   AC_MSG_RESULT([not found])
+  dnl   AC_MSG_ERROR([Please reinstall the php_tracer distribution])
+  dnl fi
+
+  dnl # --with-php_tracer -> add include path
+  dnl PHP_ADD_INCLUDE($PHP_TRACER_DIR/include)
+
+  dnl # --with-php_tracer -> check for lib and symbol presence
+  dnl LIBNAME=php_tracer # you may want to change this
+  dnl LIBSYMBOL=php_tracer # you most likely want to change this 
+
+  dnl PHP_CHECK_LIBRARY($LIBNAME,$LIBSYMBOL,
+  dnl [
+  dnl   PHP_ADD_LIBRARY_WITH_PATH($LIBNAME, $PHP_TRACER_DIR/lib, PHP_TRACER_SHARED_LIBADD)
+  dnl   AC_DEFINE(HAVE_PHP_TRACERLIB,1,[ ])
+  dnl ],[
+  dnl   AC_MSG_ERROR([wrong php_tracer lib version or lib not found])
+  dnl ],[
+  dnl   -L$PHP_TRACER_DIR/lib -lm
+  dnl ])
+  dnl
+  dnl PHP_SUBST(PHP_TRACER_SHARED_LIBADD)
+
+  PHP_NEW_EXTENSION(php_tracer, php_tracer.c, $ext_shared)
+fi
---
+++ php_tracer.c
@@ -0,0 +1,321 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2013 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:                                                              |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+#include "php_ini.h"
+#include "ext/standard/info.h"
+#include "php_php_tracer.h"
+
+#include <stdio.h>
+#include <time.h>
+/* If you declare any globals in php_php_tracer.h uncomment this:
+ZEND_DECLARE_MODULE_GLOBALS(php_tracer)
+*/
+
+/* True global resources - no need for thread safety here */
+static int le_php_tracer;
+
+/* {{{ php_tracer_functions[]
+ *
+ * Every user visible function must have an entry in php_tracer_functions[].
+ */
+const zend_function_entry php_tracer_functions[] = {
+	PHP_FE(confirm_php_tracer_compiled,	NULL)		/* For testing, remove later. */
+	PHP_FE_END	/* Must be the last line in php_tracer_functions[] */
+};
+/* }}} */
+
+/* {{{ php_tracer_module_entry
+ */
+zend_module_entry php_tracer_module_entry = {
+#if ZEND_MODULE_API_NO >= 20010901
+	STANDARD_MODULE_HEADER,
+#endif
+	"php_tracer",
+	php_tracer_functions,
+	PHP_MINIT(php_tracer),
+	PHP_MSHUTDOWN(php_tracer),
+	PHP_RINIT(php_tracer),		/* Replace with NULL if there's nothing to do at request start */
+	PHP_RSHUTDOWN(php_tracer),	/* Replace with NULL if there's nothing to do at request end */
+	PHP_MINFO(php_tracer),
+#if ZEND_MODULE_API_NO >= 20010901
+	"0.1", /* Replace with version number for your extension */
+#endif
+	STANDARD_MODULE_PROPERTIES
+};
+/* }}} */
+
+#ifdef COMPILE_DL_PHP_TRACER
+ZEND_GET_MODULE(php_tracer)
+#endif
+
+/* {{{ PHP_INI
+ */
+/* Remove comments and fill if you need to have entries in php.ini
+PHP_INI_BEGIN()
+    STD_PHP_INI_ENTRY("php_tracer.global_value",      "42", PHP_INI_ALL, OnUpdateLong, global_value, zend_php_tracer_globals, php_tracer_globals)
+    STD_PHP_INI_ENTRY("php_tracer.global_string", "foobar", PHP_INI_ALL, OnUpdateString, global_string, zend_php_tracer_globals, php_tracer_globals)
+PHP_INI_END()
+*/
+/* }}} */
+
+/* {{{ php_php_tracer_init_globals
+ */
+/* Uncomment this function if you have INI entries
+static void php_php_tracer_init_globals(zend_php_tracer_globals *php_tracer_globals)
+{
+	php_tracer_globals->global_value = 0;
+	php_tracer_globals->global_string = NULL;
+}
+*/
+/* }}} */
+
+/* {{{ PHP_MINIT_FUNCTION
+ */
+
+
+#if PHP_VERSION_ID >= 50500
+static void (*old_execute_ex)(zend_execute_data *execute_data TSRMLS_DC);
+static void tracer_execute_ex(zend_execute_data *execute_data TSRMLS_DC);
+#else
+static void (*old_execute)(zend_op_array *op_array TSRMLS_DC);
+static void tracer_execute(zend_op_array *op_array TSRMLS_DC);
+#endif
+static void op_array_traverse(zend_op_array *op_array);
+static void (*old_execute_internal)(zend_execute_data *execute_data_ptr, int return_value_used TSRMLS_DC);
+static void tracer_execute_internal(zend_execute_data *execute_data_ptr, int return_value_used TSRMLS_DC);
+
+
+clock_t start,end;
+PHP_MINIT_FUNCTION(php_tracer)
+{
+	/* If you have INI entries, uncomment these lines 
+	REGISTER_INI_ENTRIES();
+	*/
+
+	
+#if PHP_VERSION_ID>=50500
+	old_execute_ex = zend_execute_ex;
+	zend_execute_ex = tracer_execute_ex;
+#else
+	old_execute = zend_execute;
+	zend_execute = tracer_execute;
+#endif
+
+
+	old_execute_internal = zend_execute_internal;
+	zend_execute_internal = tracer_execute_internal;
+
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MSHUTDOWN_FUNCTION
+ */
+PHP_MSHUTDOWN_FUNCTION(php_tracer)
+{
+	/* uncomment this line if you have INI entries
+	UNREGISTER_INI_ENTRIES();
+	*/
+
+	
+#if PHP_VERSION_ID>=50500
+	zend_execute_ex = old_execute_ex;
+#else
+	zend_execute = old_execute;
+#endif
+
+
+
+
+	zend_execute_internal = old_execute_internal;
+
+	return SUCCESS;
+}
+/* }}} */
+
+/* Remove if there's nothing to do at request start */
+/* {{{ PHP_RINIT_FUNCTION
+ */
+PHP_RINIT_FUNCTION(php_tracer)
+{
+/*	
+	old_execute = zend_execute;
+	zend_execute = tracer_execute;
+*/
+
+
+	start = clock();
+	php_printf("-------------Request Start:  %d, loops: %d per sec\n<br/>",start,CLOCKS_PER_SEC);
+	return SUCCESS;
+
+}
+/* }}} */
+
+/* Remove if there's nothing to do at request end */
+/* {{{ PHP_RSHUTDOWN_FUNCTION
+ */
+PHP_RSHUTDOWN_FUNCTION(php_tracer)
+{
+
+	//zend_execute = old_execute;
+
+
+	end = clock();
+	php_printf("--------------Request End:  %d,",end);
+	int interval = (end - start) ;
+	php_printf(" pass: %d, interval: %f\n<br/>",interval,interval/CLOCKS_PER_SEC);
+
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MINFO_FUNCTION
+ */
+PHP_MINFO_FUNCTION(php_tracer)
+{
+	php_info_print_table_start();
+	php_info_print_table_header(2, "php_tracer support", "enabled");
+	php_info_print_table_end();
+
+	/* Remove comments if you have entries in php.ini
+	DISPLAY_INI_ENTRIES();
+	*/
+}
+/* }}} */
+
+
+static void tracer_execute(zend_op_array *op_array TSRMLS_DC)
+{
+	 
+	op_array_traverse(op_array);
+	// File *fp;
+	// fp = fopen("php://output");
+	// fprintf(fp,"executed");
+	// fclose(fp);
+	old_execute(op_array TSRMLS_CC);
+	
+}
+
+static void tracer_execute_ex(zend_execute_data *execute_data TSRMLS_DC)
+{
+
+	time_t time_start;
+	// struct tm *tm_start;
+	// char *datetime;
+	// time(&time_start);
+	// tm_start = localtime(&time_start);
+	// datetime = asctime(tm_start);
+
+	php_printf(">>>>>>>Execute start: %d  time: %s  \n<br/>",clock(),ctime(&time_start));
+
+	op_array_traverse(execute_data->op_array);
+
+
+	// File *fp;
+	// fp = fopen("php://output");
+	// fprintf(fp,"executed");
+	// fclose(fp);
+	old_execute_ex(execute_data TSRMLS_CC);
+	php_printf(">>>>>>>Execute end: %d\n<br/>",clock());
+}
+
+static void op_array_traverse(zend_op_array *op_array) {
+
+
+	int size = op_array->last;
+
+	int i;
+
+	php_printf("opcode count: %d\n<br/>",size);
+
+	zend_op *opcodes = op_array->opcodes;
+
+	for(i = 0; i <= size; i++) {
+
+		php_printf("%d: %d\n<br/>",i,opcodes[i].opcode);
+			
+	}
+
+	
+
+}
+static void tracer_execute_internal(zend_execute_data *execute_data_ptr, int return_value_used TSRMLS_DC)
+{
+
+
+	 clock_t start_internal = clock();
+	 php_printf(">>>>>>>Internal function start: %d\n<br/>",start_internal);
+
+	 php_printf("execute_internal\n<br/>");
+	 //op_array_traverse(execute_data_ptr->op_array);
+
+	 old_execute_internal(execute_data_ptr,return_value_used TSRMLS_CC);
+
+	 clock_t end_internal = clock();
+	 php_printf(">>>>>>>Internal function end: %d, ",end_internal); 
+	 clock_t interval_internal = end_internal - start_internal;
+	 php_printf("pass: %d, interval: %f\n<br/>",interval_internal,interval_internal/CLOCKS_PER_SEC);
+
+}
+
+
+
+
+/* Remove the following function when you have succesfully modified config.m4
+   so that your module can be compiled into PHP, it exists only for testing
+   purposes. */
+
+/* Every user-visible function in PHP should document itself in the source */
+/* {{{ proto string confirm_php_tracer_compiled(string arg)
+   Return a string to confirm that the module is compiled in */
+PHP_FUNCTION(confirm_php_tracer_compiled)
+{
+	char *arg = NULL;
+	int arg_len, len;
+	char *strg;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg, &arg_len) == FAILURE) {
+		return;
+	}
+
+	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "php_tracer", arg);
+	RETURN_STRINGL(strg, len, 0);
+}
+/* }}} */
+/* The previous line is meant for vim and emacs, so it can correctly fold and 
+   unfold functions in source code. See the corresponding marks just before 
+   function definition, where the functions purpose is also documented. Please 
+   follow this convention for the convenience of others editing your code.
+*/
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
